109ce9414e9bbe6eebfc0a6012b4e22ec8310fda
Elisabeth Irgens <hello@elisabethirgens.com>
2017-12-05 23:25:28 +0100

    Templatify start of day view 🌅

--- script-mustache.js
+++ script-mustache.js
@@ -7,10 +7,13 @@ function loadTemplate(templateName){
 }
 
 var view = {
-  title: "Joe",
-  calc: function () {
-    return 2 + 4;
-  }
+  "events": [
+    {"hour":6},
+    {"hour":7},
+    {"hour":8, "title":"demo", "location": "M1802", "participants": "3 accepted"},
+    {"hour":9, "title":"developer only", "location": "M1702", "participants": "30 accepted, 2 declined, 5 pending"},
+    {"hour":10}]
+
 };
 
 var output = Mustache.render(loadTemplate("template.html"), view);
