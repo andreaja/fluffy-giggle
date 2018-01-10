51e80beef1848c92ce3ca08b1a5829f8a932191f
Elisabeth Irgens <hello@elisabethirgens.com>
2018-01-10 21:33:13 +0100

    Define a current day and add a list of next days 🔜

--- script-calendar.js
+++ script-calendar.js
@@ -6,6 +6,8 @@ function loadTemplate(templateName){
   return fs.readFileSync(templateName).toString();
 }
 
+const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 var view = { };
 
 var hours = [ ];
@@ -16,6 +18,17 @@ for (var i = 6; i < 22; i++){
 
 view.events = hours;
 
+var viewingdate = new Date('2018-01-10T00:00:00+01:00');
+
+var weekdays = [ ];
+
+for (var i = 0; i < 7; i++){
+    var d = new Date(viewingdate);
+    d.setDate(d.getDate() + 1 + i);
+    weekdays.push({'day': `${week[d.getDay()]} ${d.getDate()}/${d.getMonth()+1} `});
+}
+
+view.weekday = weekdays;
 
 var output = Mustache.render(loadTemplate("template.html"), view);
 console.log (output);
